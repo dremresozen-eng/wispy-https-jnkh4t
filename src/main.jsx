d1dd1a6c2fcc7909f01229eea3ea36c0d8fc0c15
dremresozen-eng <dr.emresozen@gmail.com>
2025-11-08 22:53:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Eğer App başka yerdeyse yolu ona göre düzelt
+import App from "./App.jsx";
+ // Eğer App başka yerdeyse yolu ona göre düzelt
 // import "./index.css"; // CSS dosyan varsa bunu aç
 
 ReactDOM.createRoot(document.getElementById("root")).render(
