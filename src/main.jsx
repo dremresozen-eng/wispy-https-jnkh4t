991275f86fefb6c4998a66236fba34c32985d837
dremresozen-eng <dr.emresozen@gmail.com>
2025-11-09 21:27:48 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
- // Eğer App başka yerdeyse yolu ona göre düzelt
-// import "./index.css"; // CSS dosyan varsa bunu aç
+import "./styles.css";  // ← THIS LINE IS CRITICAL!
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
